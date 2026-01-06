48f45834e54c36c8df2b865ff70f68e25cd98727
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-01-06 15:40:12 +0000

    update

--- src/middleware.js
+++ src/middleware.js
@@ -4,6 +4,7 @@ import { NextResponse } from 'next/server';
 
 export function middleware(req) {
     const token = req.cookies.get('DOKTA_ACCESS_TOKEN')?.value;
+    console.log("Middlewqare token: ", token)
     const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
     if (isProtectedRoute && !token) {
         return NextResponse.redirect(new URL('/auth/login', req.url));
