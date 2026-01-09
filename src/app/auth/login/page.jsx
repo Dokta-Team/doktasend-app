41a6c40df088763389f25d24123c2c21ac964ad2
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-01-09 22:06:41 +0000

    still fixing the console and API_URL issue

--- src/app/auth/login/page.jsx
+++ src/app/auth/login/page.jsx
@@ -36,7 +36,8 @@ export default function Login() {
   };
 
 
-  const baseURL = process.env.NEXT_PUBLIC_API_URL1;
+  const baseURL = process.env.NEXT_PUBLIC_API_URL;
+  console.log("API Base URL", "baseURL");
   console.log("API Base URL: 1d2", baseURL);
   // const handleSubmit = async (e) => {
   //   try {
