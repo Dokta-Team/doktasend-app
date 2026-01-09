1e4242cdc9de3177b16fb7e9afe0f26d361c5223
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-01-09 21:48:42 +0000

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -5,7 +5,6 @@ import { NextResponse } from 'next/server';
 export function middleware(req) {
     const token = req.cookies.get('DOKTA_ACCESS_TOKEN')?.value;
     console.log("Middlewqare NEXT_PUBLIC_API_URL: ", token)
-    console.log("Middlewqare NEXT_PUBLIC_API_URL: ", process.env.NEXT_PUBLIC_API_URL ? process.env.NEXT_PUBLIC_API_URL : 'process.env.NEXT_PUBLIC_API_URL not set')
     const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
     if (isProtectedRoute && !token) {
         return NextResponse.redirect(new URL('/auth/login', req.url));
