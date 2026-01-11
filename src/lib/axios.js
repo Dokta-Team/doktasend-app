2e290df2aab1abb6c2ea21eb0d3bcd57b28c81b4
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-01-11 19:34:55 +0000

    Update axios.js

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,7 @@
 import axios from 'axios';
 
-const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5200/api/v1';
+// const baseURL = process.env.NEXT_PUBLIC_API_URL || 'http://localhost:5200/api/v1';
+const baseURL = process.env.NEXT_PUBLIC_API_URL || 'https://api.doktasend.com/api/v1';
 
 const api = axios.create({
   baseURL,
