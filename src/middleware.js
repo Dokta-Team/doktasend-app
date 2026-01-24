999bb655e020a98df2f481ebe7545d3cc1cb5a1c
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-01-24 11:09:53 +0000

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -3,14 +3,18 @@ import { NextResponse } from 'next/server';
 
 
 export function middleware(req) {
-    const token = req.cookies.get('DOKTA_ACCESS_TOKEN')?.value;
-    console.log("Middlewqare back: ", token)
-    const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
-    if (isProtectedRoute && !token) {
-        return NextResponse.redirect(new URL('/auth/login', req.url));
-    }
+    try {
+        const token = req.cookies.get('DOKTA_ACCESS_TOKEN')?.value;
+        console.log("Middlewqare back: ", token)
+        const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
+        if (isProtectedRoute && !token) {
+            return NextResponse.redirect(new URL('/auth/login', req.url));
+        }
 
-    return NextResponse.next();
+        return NextResponse.next();
+    } catch (error) {
+        console.error('Middleware error:', error);
+    }
 }
 
 export const config = {
