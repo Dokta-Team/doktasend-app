144651489dcb3cac907333edaf3ca05e1406c273
Dolapo Sokoya <65714464+dolaposokoya@users.noreply.github.com>
2026-06-08 10:09:51 +0100

    Update middleware.js

--- src/middleware.js
+++ src/middleware.js
@@ -1,22 +1,43 @@
-// middleware.ts in Next.js
-import { NextResponse } from 'next/server';
+import { NextRequest, NextResponse } from 'next/server';
+
+
+// export function middleware(req) {
+//     try {
+//         const token = req.cookies.get('DOKTA_SEND_ACCESS_TOKEN')?.value;
+//         console.log("Middlewqare back: ", token)
+//         const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
+//         if (isProtectedRoute && !token) {
+//             return NextResponse.redirect(new URL('/auth/login', req.url));
+//         }
+
+//         return NextResponse.next();
+//     } catch (error) {
+//         console.error('Middleware error:', error);
+//     }
+// }
 
 
 export function middleware(req) {
     try {
-        const token = req.cookies.get('DOKTA_SEND_ACCESS_TOKEN')?.value;
-        console.log("Middlewqare back: ", token)
-        const isProtectedRoute = req.nextUrl.pathname.startsWith('/dashboard') || req.nextUrl.pathname.startsWith('/admin');
+        const token =
+            req.cookies.get('DOKTA_SEND_ACCESS_TOKEN')?.value;
+
+        const isProtectedRoute =
+            req.nextUrl.pathname.startsWith('/dashboard') ||
+            req.nextUrl.pathname.startsWith('/admin');
+
         if (isProtectedRoute && !token) {
-            return NextResponse.redirect(new URL('/auth/login', req.url));
+            return NextResponse.redirect(
+                new URL('/auth/login', req.url)
+            );
         }
 
         return NextResponse.next();
     } catch (error) {
         console.error('Middleware error:', error);
+        return NextResponse.next();
     }
 }
-
 
 export const config = {
     matcher: ['/dashboard/:path*', '/admin/:path*'],
